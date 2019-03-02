e633d5be13a7e6c4ff1a2c4640d8725863c85749
Unknown <joakim.kronqvist@aalto.fi>
2019-03-02 13:17:50 +0200

    fix ts-linting

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,36 +1,31 @@
 import * as Phaser from 'phaser';
 
-//tslint:disable
-
 class MyScene extends Phaser.Scene {
-        
-    preload ()
-    {
+    preload = () => {
         this.load.setBaseURL('http://labs.phaser.io');
-    
+
         this.load.image('sky', 'assets/skies/space3.png');
         this.load.image('logo', 'assets/sprites/phaser3-logo.png');
         this.load.image('red', 'assets/particles/red.png');
     }
-    
-    create ()
-    {
+
+    create = () => {
         this.add.image(400, 300, 'sky');
-    
-        var particles = this.add.particles('red');
-    
-        var emitter = particles.createEmitter({
+
+        const particles = this.add.particles('red');
+
+        const emitter = particles.createEmitter({
             speed: 100,
             scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
+            blendMode: 'ADD',
         });
-    
-        var logo = this.physics.add.image(400, 100, 'logo');
-    
+
+        const logo = this.physics.add.image(400, 100, 'logo');
+
         logo.setVelocity(100, 200);
         logo.setBounce(1, 1);
         logo.setCollideWorldBounds(true);
-    
+
         emitter.startFollow(logo);
     }
 }
@@ -43,8 +38,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
-        }
+            gravity: { y: 200 },
+        },
     },
     scene: MyScene,
 };
