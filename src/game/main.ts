2d9d3a35a9bbd31ca4928af3aaab2db2ea8634f2
Hanne Aho <haah@netlight.com>
2019-03-02 16:19:28 +0200

    Add player class

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,32 +1,29 @@
 import * as Phaser from 'phaser';
+import Player from './player';
 
 class MyScene extends Phaser.Scene {
-    preload = () => {
-        this.load.setBaseURL('http://labs.phaser.io');
+    player: Phaser.GameObjects.Image;
+    keys: Phaser.Input.Keyboard.CursorKeys;
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+    preload = () => {
+        this.load.setPath('images/');
+        this.load.image('player', 'mario.png');
     }
 
     create = () => {
-        this.add.image(400, 300, 'sky');
-
-        const particles = this.add.particles('red');
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD',
+        this.player = new Player({
+            scene: this,
+            x: 400,
+            y: 300,
+            texture: 'player',
         });
+        this.player.setScale(.2);
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+        this.keys = this.input.keyboard.createCursorKeys();
+    }
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        emitter.startFollow(logo);
+    update = () => {
+        this.player.update(this.keys, 3);
     }
 }
 
